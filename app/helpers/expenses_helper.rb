4ad4d556591b6d1ffac0582eea32ba2515dd6569
Xozi <khozaima.mahmoodi@gmail.com>
2026-03-08 00:45:45 +0400

    Add category color helper and Style expense badges

--- app/helpers/expenses_helper.rb
+++ app/helpers/expenses_helper.rb
@@ -1,2 +1,14 @@
 module ExpensesHelper
+  CATEGORY_COLORS = {
+    "food"        => "bg-orange-100 text-orange-600",
+    "transport"   => "bg-blue-100 text-blue-600",
+    "health"      => "bg-green-100 text-green-600",
+    "shopping"    => "bg-pink-100 text-pink-600",
+    "utilities"   => "bg-yellow-100 text-yellow-600",
+    "entertainment" => "bg-purple-100 text-purple-600"
+  }.freeze
+
+  def category_color_class(category)
+    CATEGORY_COLORS.fetch(category.to_s.downcase, "bg-gray-100 text-gray-600")
+  end
 end
